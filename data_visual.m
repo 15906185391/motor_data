7bfd7746bbe12fda95c49a720c125e299520a40d
15906185391 <76636159+15906185391@users.noreply.github.com>
2023-07-19 18:44:21 +0800

    Add files via upload

--- data_visual.m
+++ data_visual.m
@@ -1,2 +1,6 @@
-T = readtable('0.csv','Range','A5:A87045');
-T
+T = readtable('0.csv','Range','A6:A87045');
+T2=timetable(T.Var1,'SampleRate',1024);
+T2
+T3 = readtable('1.csv','Range','A6:A70661');
+T4=timetable(T3.Var1,'SampleRate',1024);
+T4
